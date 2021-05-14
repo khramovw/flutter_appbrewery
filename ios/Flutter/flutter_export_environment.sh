068f3ad71f8c2397bfee50bb382b084c321e7de0
Vladislav Khramov <khramovw@gmail.com>
2021-05-14 21:50:56 +0300

    upd

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/khramovw/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/khramovw/STUDY/appbrewery"
-export "FLUTTER_TARGET=/Users/khramovw/STUDY/appbrewery/lib/main.dart"
+export "FLUTTER_ROOT=/Users/khramov/Develp/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/khramov/Develp/study/flutter_appbrewery"
+export "FLUTTER_TARGET=/Users/khramov/Develp/study/flutter_appbrewery/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/khramovw/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/khramov/Develp/study/flutter_appbrewery/.dart_tool/package_config.json"
